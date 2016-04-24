ef746911f6cd3fa7761866f93b73700ac7352e4f
Basheer Becerra <bbecerr@outlook.com>
2016-04-24 10:54:46 -0500

    Completed test EventControllerTest#testEventDetails()
    -Made changes to EventController#eventDetails()

--- programmunitywebapplication/src/main/java/com/programmunity/webapplication/controllers/EventsController.java
+++ programmunitywebapplication/src/main/java/com/programmunity/webapplication/controllers/EventsController.java
@@ -1,13 +1,17 @@
 package com.programmunity.webapplication.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.programmunity.webapplication.database.EventRepository;
+import com.programmunity.webapplication.models.Event;
 
 /**
  * Controls event mappings
@@ -53,8 +57,8 @@ public class EventsController extends BaseController
 	 *            of event
 	 * @return {@link ModelAndView}
 	 */
-	@RequestMapping(value = "/details", method = RequestMethod.GET)
-	public ModelAndView eventDetails(@RequestParam(value = "eventId") long eventId)
+	@RequestMapping(value = "/event/{eventId}", method = RequestMethod.GET)
+	public ModelAndView eventDetails(@PathVariable(value = "eventId") long eventId)
 	{
 		ModelAndView mav = new ModelAndView("event");
 		bindContentToModel(mav);
@@ -62,9 +66,10 @@ public class EventsController extends BaseController
 		return mav;
 	}
 
-	// Need validation and CSRF
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public ModelAndView register(@RequestParam(value = "eventId") long eventId,
+	// TODO: Correct the mapping of this
+	// TODO: Need validation and CSRF
+	@RequestMapping(value = "/event/{eventId}/register", method = RequestMethod.POST)
+	public ModelAndView eventRegister(@PathVariable(value = "eventId") long eventId,
 			@RequestParam(value = "userId") long userId)
 	{
 		ModelAndView mav = new ModelAndView("redirect: /events?eventId=" + eventId);
--- programmunitywebapplication/src/test/java/com/programmunity/webapplication/controllers/EventControllerTest.java
+++ programmunitywebapplication/src/test/java/com/programmunity/webapplication/controllers/EventControllerTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -28,6 +29,12 @@ import com.programmunity.webapplication.models.GroupRole;
 import com.programmunity.webapplication.models.Residable;
 import com.programmunity.webapplication.models.User;
 
+/**
+ * Tests the {@link EventController} class
+ * 
+ * @author Basheer
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
 public class EventControllerTest
@@ -42,7 +49,12 @@ public class EventControllerTest
 	private MockMvc mockMvc;
 
 	private List<Event> expectedEvents;
-	int count = 20;
+
+	private Event expectedEvent;
+
+	private static final int count = 20;
+
+	private static final long eventId = 12345L;
 
 	/**
 	 * Initiate the mocks
@@ -63,14 +75,21 @@ public class EventControllerTest
 
 		expectedEvents = createEventList(count);
 
+		expectedEvent = createEvent();
+
 		/*
 		 * Set return of event repository to the mock event list
 		 * 
 		 * !IMPORTANT: The arguments are from the default values of the
 		 * controller method
 		 */
-		Mockito.when(eventRepository.getEvents(1, "newest", 20)).thenReturn(expectedEvents);
+		Mockito.when(eventRepository.getEvents(1, "newest", count)).thenReturn(expectedEvents);
 
+		/*
+		 * Set return of event repository to the mock event
+		 * 
+		 */
+		Mockito.when(eventRepository.getEvent(eventId)).thenReturn(expectedEvent);
 		/*
 		 * Build the mock mvc
 		 */
@@ -81,7 +100,7 @@ public class EventControllerTest
 	/**
 	 * Tests method mapping and view name
 	 * 
-	 * @method {@link EventsController#eventList()}
+	 * @method {@link EventsController#eventList(int, String, int)}
 	 * @perform Send a mock GET "/events/"
 	 * 
 	 * @expectedResults Receives correct view name "events"
@@ -107,6 +126,66 @@ public class EventControllerTest
 
 	}
 
+	/**
+	 * Tests method mapping and view name
+	 * 
+	 * @method {@link EventsController#eventDetails(long)}
+	 * @perform Send a mock GET "/events/event/{eventId}/"
+	 * 
+	 * @expectedResults Receives correct view name "event"
+	 * @expectedResults HTTP OK
+	 * @expectedResults Correct forward url "event"
+	 * @expectedResults Has an "event" attribute
+	 * @expectedResults "event" attribute is same as mock
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testEventDetails() throws Exception
+	{
+		Map<String, Object> requestParameters = null;
+
+		/*
+		 */
+		mockMvc.perform(MockMvcRequestBuilders.get(constructRequest("/events/event/" + eventId, requestParameters)))
+				.andExpect(MockMvcResultMatchers.view().name("event")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.forwardedUrl("event"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("event"))
+				.andExpect(MockMvcResultMatchers.model().attribute("event", Matchers.equalTo(expectedEvent)));
+
+	}
+
+	// TODO: Complete the eventRegister test
+	/**
+	 * Tests method mapping and view name
+	 * 
+	 * @method {@link EventsController#eventDetails(long)}
+	 * @perform Send a mock GET "/events/event/{eventId}/register"
+	 * 
+	 * @expectedResults Receives correct view name "event"
+	 * @expectedResults HTTP OK
+	 * @expectedResults Correct forward url "event"
+	 * @expectedResults Has an "event" attribute
+	 * @expectedResults "event" attribute is same as mock
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	@Ignore
+	public void testEventRegister() throws Exception
+	{
+		Map<String, Object> requestParameters = null;
+
+		/*
+		 */
+		mockMvc.perform(MockMvcRequestBuilders.get(constructRequest("/events/event/" + eventId, requestParameters)))
+				.andExpect(MockMvcResultMatchers.view().name("event")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.forwardedUrl("event"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("event"))
+				.andExpect(MockMvcResultMatchers.model().attribute("event", Matchers.equalTo(expectedEvent)));
+
+	}
+	
 	/**
 	 * Create the mock event list
 	 * 
@@ -153,6 +232,45 @@ public class EventControllerTest
 			eventList.add(event);
 		}
 		return eventList;
+	}
+
+	/**
+	 * Creates a mock event
+	 * @return
+	 */
+	private Event createEvent()
+	{
+		Event event = new Event();
+		event.setTitle("Title: ");
+		event.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+		List<Residable> attendees = new ArrayList<Residable>();
+
+		Group group = new Group();
+		Directory directory = group.new Directory();
+		Map<User, GroupRole> members = new HashMap<User, GroupRole>();
+
+		User user1 = new User();
+		user1.setUserName("user1_");
+		User user2 = new User();
+		user2.setUserName("user2_");
+		User user3 = new User();
+		user3.setUserName("user3_");
+		User user4 = new User();
+		user4.setUserName("user4_");
+		User user5 = new User();
+		user5.setUserName("user5_");
+
+		directory.setOwner(user1);
+		members.put(user2, GroupRole.ADMINISTRATOR);
+		members.put(user3, GroupRole.MODERATOR);
+		members.put(user4, GroupRole.REGULAR);
+		members.put(user5, GroupRole.REGULAR);
+		directory.setMembers(members);
+		group.setDirectory(directory);
+		attendees.add(group);
+
+		event.setAttendees(attendees);
+		return event;
 	}
 
 	/**
@@ -203,7 +321,7 @@ public class EventControllerTest
 			}
 		}
 		requestBuilder.append("/");
-		
+
 		return requestBuilder.toString();
 	}
 
